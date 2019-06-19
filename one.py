223aa4950c5a12b8cec23f3a46e856a51bab8a45
Sayang99 <51100872+Sayang99@users.noreply.github.com>
2019-06-20 05:19:44 +0700

    Create one.py

---
+++ one.py
@@ -0,0 +1,6 @@
+os.system('clear')
+print "[+] Compile & Decompile Python \n[+] Coded By Ahmad Riswanto \n[+] Thank you for supporting me"
+raw_input('[^] Press enter to continue ')
+os.system('clear')
+print "[+] Loading Please Wait... "
+os.system('python2 data/com.py')
